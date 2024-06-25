39d7c074ae3a3d28cd8bd87b7d533ba77910e80f
Ogame3334 <kazuhiroisobe0522@gmail.com>
2024-06-25 16:01:02 +0900

    実験ログ

---
+++ src/app/api/class/result/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+// ローカルファイルのパスを定義
+const logFilePath = join(process.cwd(), './public/logs.html');
+
+export async function POST(request: NextRequest) {
+  try {
+    // リクエストのbodyを取得
+    const body = await request.text();
+
+    // bodyの内容をlogs.txtに追記
+    let html = `<div style=\"border: 1px solid black; padding: 10px; margin: 10px;\">${body.replaceAll('\n', '<br>')}</div>`
+    await fs.appendFile(logFilePath, html + '\n');
+
+    return NextResponse.json({ message: 'Log saved successfully' });
+  } catch (error) {
+    console.error('Error saving log:', error);
+    return NextResponse.json({ error: 'Failed to save log' }, { status: 500 });
+  }
+}
